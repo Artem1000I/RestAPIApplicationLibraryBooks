6c0a5c1dac048ee550cf88466c146d608e1a8ee8
Artem Tysychniy <artish111artish@gmail.com>
2023-05-09 00:29:24 +0300

    Books

--- src/test/java/AbstractTest.java
+++ src/test/java/AbstractTest.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class AbstractTest {
+import io.cucumber.java.BeforeAll;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AbstractTest {
+    static Properties prop = new Properties();
+    private static InputStream configFile;
+    private static String baseUrl; //Урл сайта или ресурса
+
+    @BeforeAll //Будет применяться для всех тестов
+    static void initTest() throws IOException {
+        configFile = new FileInputStream("src/main/resources/my.properties");// cчитываем конфигурационный файл
+        prop.load(configFile); //загружаем его в обьект пропертис prop
+
+
+        baseUrl= prop.getProperty("base_url");//достаем значение которое будем использовать
+    }
+
+    public static String getBaseUrl() {
+        return baseUrl;
+    }
 }
